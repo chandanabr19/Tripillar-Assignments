0765a0dbf20a014cf4522a4fecd4ceb8f9bb0f63
chandanabr19 <141708307+chandanabr19@users.noreply.github.com>
2026-03-12 11:56:41 +0530

    Add files via upload

---
+++ Module 2/Palindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Palindrome{
+
+    
+    static boolean isPalindrome(String str) {
+        str = str.toLowerCase(); // make it case-insensitive
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
